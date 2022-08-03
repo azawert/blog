352145d389fe6614e0bb547bf6aa4025b47c9ae1
azawert <azawertik@gmail.com>
2022-08-03 18:03:00 +0300

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
-
+import cors from "cors";
 import {
   registerValidator,
   loginValidator,
@@ -30,7 +30,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-
+app.use(cors());
 app.use(express.json());
 app.use("/upload", express.static("uploads"));
 
